914d052c2266f539f0af8865918a1d3b22551a19
ozihler <oliver.zihler@zuehlke.com>
2018-05-28 12:04:37 +0200

    ccws-2018: update ColorTest to use JUnit5.

--- tests/com/zuehlke/cleancodeworkshop/smellyshapes/ColorTest.java
+++ tests/com/zuehlke/cleancodeworkshop/smellyshapes/ColorTest.java
@@ -1,12 +1,15 @@
 package com.zuehlke.cleancodeworkshop.smellyshapes;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("A color")
 public class ColorTest {
 
     @Test
+    @DisplayName("returns an error message if the color given as text is written incorrectly")
     public void getErrorMessage_invalidColor() {
         Color c = new Color("INVALIDColor_N4me");
 
@@ -14,6 +17,7 @@ public class ColorTest {
     }
 
     @Test
+    @DisplayName("returns an error message if the color given as text is not an existing color (Magenta)")
     public void getErrorMessage_Magenta() {
         Color c = new Color("Magenta");
 
@@ -21,6 +25,7 @@ public class ColorTest {
     }
 
     @Test
+    @DisplayName("returns an error message if the color given as text is not an existing color (Cyan)")
     public void getErrorMessage_Cyan() {
         Color c = new Color("Cyan");
 
@@ -29,6 +34,7 @@ public class ColorTest {
 
 
     @Test
+    @DisplayName("returns a string containing hex and rgb values if its a recognized color (Red)")
     public void getColorFormatted_true() {
         Color c = new Color("Red");
 
@@ -38,6 +44,7 @@ public class ColorTest {
     }
 
     @Test
+    @DisplayName("returns a string only containing its name if its a recognized color (Red) and should not include hex and rgb")
     public void getColorFormatted_false() {
         Color c = new Color("Red");
 
