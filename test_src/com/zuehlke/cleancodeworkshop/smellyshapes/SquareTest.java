428c4965ab0247093aac1882f87a9d156345b718
ozihler <oliver.zihler@zuehlke.com>
2018-05-28 10:47:08 +0200

    ccws-2018: update first test to use DisplayName.

--- test_src/com/zuehlke/cleancodeworkshop/smellyshapes/SquareTest.java
+++ test_src/com/zuehlke/cleancodeworkshop/smellyshapes/SquareTest.java
@@ -1,26 +1,31 @@
 package com.zuehlke.cleancodeworkshop.smellyshapes;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@DisplayName("A square")
 public class SquareTest {
 
     @Test
+    @DisplayName("calculates its area according to edge length")
     public void calculateArea() {
         Square square = new Square(0, 0, 2);
         assertEquals(4, square.calculate());
     }
 
     @Test
+    @DisplayName("prints its contents")
     public void toString_() {
         Square square = new Square(0, 0, 1, new Color("Red"));
         assertEquals("Square: (0,0) edgeLength=1 color=#FF0000", square.toString());
     }
 
     @Test
+    @DisplayName("can be converted to xml")
     public void toXml() {
         Square square = new Square(0, 1, 2);
         String xml = square.toXml();
@@ -28,6 +33,7 @@ public class SquareTest {
     }
 
     @Test
+    @DisplayName("can determine if it contains points")
     public void containsPoints() {
         Square square = new Square(0, 0, 1);
 
@@ -43,6 +49,7 @@ public class SquareTest {
         assertFalse(square.containsPoint(2, 1));
     }
 
+    @DisplayName("throws an exception when getHeight() is called")
     @Test(expected = RuntimeException.class)
     public void getHeigth() {
         new Square(0, 0, 0).getHeight();
